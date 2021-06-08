fe7487220c39483217c065aec51d228722224baf
SilviuCelMare <silviuflorin12@gmail.com>
2021-06-08 18:55:48 +0300

    adding node server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
+// librarie care inlocuieste fetch
+import axios from 'axios';
+
 import StripeCheckout from 'react-stripe-checkout';
+
 
 const StripeCheckoutButton = ({price}) => {
     const priceStripe = price * 100;
     const publishableKey = 'pk_test_51IyxdNIwwFVm366iaqGxwNCvC9j6eo77LIlqollBocOes0bh9CNnOKRkbZcWyc96lAgsIx7cKAzAtFojmR7eiFGf00wBNZjkrn';
+    
     const onToken = token =>{
-        console.log(token);
-        alert('Tranzacție realizată cu succes!');
+        axios({
+            url:'payment',
+            method:'post',
+            data:{
+                amount:priceStripe,
+                token
+            }
+        }).then(response => {
+            alert('Plată realizată cu succes!')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('A apărut o eroare! Plata dumneavoastră nu a fost procesată!');
+        })
     }
+
     return(
         <StripeCheckout 
         currency="ron"
